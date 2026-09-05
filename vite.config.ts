50be905641123e31894df26746d25394aed0a042
yashi <yashirastogi263@gmail.com>
2026-09-05 22:39:34 +0530

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -12,4 +12,10 @@ export default defineConfig({
     // nitro/vite builds from this
     server: { entry: "server" },
   },
+  vite: {
+    preview: {
+      host: true,
+      allowedHosts: ["marketpulse-frontend-rit9.onrender.com"],
+    },
+  },
 });
